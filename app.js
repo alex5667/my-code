06f8ff1967237317049b72179c9933f2f1f9ddb4
alex5667 <alex56671111@gmail.com>
2023-04-20 12:44:34 +0300

    refactor: according to rule 3.2 Meaningful names of identifiers and classes from html-and-css

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
-  var containsClass=listItem.classList.contains("edit-mode");
-  //If class of the parent is .edit-mode
+  var containsClass=listItem.classList.contains("task__edit-mode");
+  //If class of the parent is .task__edit-mode
   if(containsClass){
 
-    //switch to .edit-mode
+    //switch to .task__edit-mode
     //label becomes the inputs value.
     label.innerText=editInput.value;
     editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
     editBtn.innerText="Save";
   }
 
-  //toggle .edit-modeon the parent.
-  listItem.classList.toggle("edit-mode");
+  //toggle .task__edit-modeon the parent.
+  listItem.classList.toggle("task__edit-mode");
 };
 
 
